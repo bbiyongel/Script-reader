012738d6fcf3edba43e09579e4bf7dc0ae86b044
sauber92 <jjy920517@gmail.com>
2017-05-03 21:51:18 +0900

    POST api 개발 AJAX를 통해 body.ejs에서 입력 받은 데이터를 서버로 전송 서버에서는 body-parser 등을 이용하여 request로 api 서버로 전송 api secret_id를 변경함

--- router/main.js
+++ router/main.js
@@ -1,24 +1,35 @@
-var pwconfig = require('../config/password');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 var request = require('request');
+var pwconfig = require('../config/password');
 
 module.exports = function(app, fs) {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+
   app.get('/', function(req, res) {
     res.render('index', {
       title: "Script-reader"
     });
   });
 
-  app.post('/tts', function (req, res) {
-    var data = {
-      'speaker': req.body.speaker,
-      'speed': req.body.speed,
-      'text': req.body.text
-    };
+  app.post('/tts', upload.array(), function (req, res) {
     var api_url = 'https://openapi.naver.com/v1/voice/tts.bin';
+    var speaker = req.body.speaker;
+    var speed = req.body.speed;
+    var text = req.body.text;
     var options = {
       url: api_url,
-      form: {'speaker': data.speaker, 'speed': data.speed, 'text': data.text},
-      headers: {'X-Naver-Client-Id': pwconfig.client_id, 'X-Naver-Client-Secret': pwconfig.client_secret}
+      form: {
+        'speaker': speaker,
+        'speed': speed,
+        'text': text
+      },
+      header: {
+        'X-Naver-Client-Id': pwconfig.client_id,
+        'X-Naver-Client-Secret': pwconfig.client_secret
+      }
     };
     var writeStream = fs.createWriteStream('./tts1.mp3');
     var _req = request.post(options).on('response', function(response) {
